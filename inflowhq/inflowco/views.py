3efe014df0c88572c2404935e0fbb2bb8a2eb745
Brian Katchmar <brian@workinflow.co>
2017-11-08 00:51:03 -0500

    Bucket Creation and file ACL setting to start allowing us to preview deliverables

--- inflowhq/inflowco/views.py
+++ inflowhq/inflowco/views.py
@@ -1,6 +1,9 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals # I have no idea what this even is
+# References from our own library
 from accounts.linkedincalls import LinkedInApi
 from accounts.models import UserLinkedInInformation, UserSettings
+from inflowco.models import Currency
+# Django references
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
@@ -8,35 +11,38 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.utils.http import urlquote
 from django.views.generic import TemplateView
-from inflowco.models import Currency
+# Entire Libraries where we are not extracting specific things
 import boto3
+import botocore
+import urllib.parse
 
 class LoginView(TemplateView):
     template_name = "login.html"
     
     def get(self, request):
-        context = {'message':''}
+        # Log out current user if the query string has "logout" on it
         if request.GET.get("logout","") != "":
             logout(request)
-        return render(request, self.template_name, context)
+            
+        return render(request, self.template_name)
     
     def post(self, request):
-        context = {'message':''}
-        username = request.POST.get("username", "")
+        # Collect POST data
+        user_name = request.POST.get("username", "")
         password = request.POST.get("password", "")
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=user_name, password=password)
         
         if user is not None:
             login(request, user)
-            return redirect('/inflow/currencies/')
+            return redirect("/inflow/currencies/")
         
-        return render(request, self.template_name, context)
+        return render(request, self.template_name)
     
 class LinkedInHandler(TemplateView):
     template_name = 'linkedin.html'
     
     def get(self, request):
-        context = {'message':''}
+        context = {}
         
         # Getting info from the request
         linkedinerror = request.GET.get('error', '')
@@ -89,21 +95,19 @@ class LinkedInHandler(TemplateView):
                     userLinkedInProfileInfo.LinkedInAccessToken = collectedaccesstoken
                     userLinkedInProfileInfo.save()
             else:
-                newlycreateduser = User.objects.create(username=apilinkedininfo["emailAddress"],email=apilinkedininfo["emailAddress"],first_name=apilinkedininfo["firstName"],last_name=apilinkedininfo["lastName"],is_staff=False,is_active=True,is_superuser=False)
-                newlycreateduser.set_password("linkedinprofilenopasswordneeded")
-                newlycreateduser.save()
+                linkedin_profile_information = UserLinkedInInformation.objects.filter(LinkedInProfileID=apilinkedininfo["id"]).first()
                 
-                UserLinkedInInformation.objects.create(UserAccount=newlycreateduser,LinkedInProfileID=apilinkedininfo["id"],LinkedInAccessToken=collectedaccesstoken)
+                if linkedin_profile_information is None:
+                    newlycreateduser = User.objects.create(username=apilinkedininfo["emailAddress"],email=apilinkedininfo["emailAddress"],first_name=apilinkedininfo["firstName"],last_name=apilinkedininfo["lastName"],is_staff=False,is_active=True,is_superuser=False)
+                    newlycreateduser.set_password("linkedinprofilenopasswordneeded")
+                    newlycreateduser.save()
+                    UserLinkedInInformation.objects.create(UserAccount=newlycreateduser,LinkedInProfileID=apilinkedininfo["id"],LinkedInAccessToken=collectedaccesstoken)
                 
                 user = authenticate(request, username=apilinkedininfo["emailAddress"], password="linkedinprofilenopasswordneeded")
                 if user is not None:
                     login(request, user)
                     return redirect("/inflow/account/")
         
-        return render(request, self.template_name, context)
-    
-    def post(self, request):
-        context = {'message':''}
         return render(request, self.template_name, context)
     
 class CurrencyListView(LoginRequiredMixin, TemplateView):
@@ -116,33 +120,52 @@ class CurrencyListView(LoginRequiredMixin, TemplateView):
         # Call the base implementation first to get a context
         context = super(CurrencyListView, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the currencies
-        context['currencies'] = self.get_queryset()
+        context["currencies"] = self.get_queryset()
         return context
 
 class AmazonBotoExamples(LoginRequiredMixin, TemplateView):
     template_name = "boto3.html"
     
     def get(self, request):
-        context = {"message":""}
-        amazonCaller = boto3.resource("s3")
-        bucketNames = []
-        deliverableBucket = amazonCaller.Bucket("inflow-deliverables-2")
-        
-        for object in deliverableBucket.objects.all():
-            bucketNames.append(object.key)
-            
-        context["bucketNames"] = bucketNames
-        context["folderName"] = request.user.username
-        
+        # Set the Context
+        context = self.get_context_data(request)
         return render(request, self.template_name, context)
     
     def post(self, request):
-        context = {}
-        amazonCaller = boto3.resource("s3")
-        deliverableBucket = amazonCaller.Bucket("inflow-deliverables-2")
-        uploadedDeliverable = request.FILES.get("deliverable", False)
+        # Set the Context
+        context = self.get_context_data(request)
         
-        if uploadedDeliverable != False:
-            deliverableBucket.put_object(Key=("%s/%s" % (request.user.username, uploadedDeliverable.__str__())), Body=uploadedDeliverable)
+        # Get data from the POST
+        uploaded_deliverable = request.FILES.get("deliverable", False)
+        
+        # If the user actually send something over, lets capture it and upload it to S3
+        if uploaded_deliverable != False:
+            deliverable_key = uploaded_deliverable.__str__()
+            context["deliverable_bucket"].put_object(Key=deliverable_key, Body=uploaded_deliverable)
+            context["file_names"].append({"key":deliverable_key,"qs":urllib.parse.quote_plus(deliverable_key)})
         
         return render(request, self.template_name, context)
+    
+    def get_context_data(self, request, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(AmazonBotoExamples, self).get_context_data(**kwargs)
+        
+        # Instantiate items related to the context
+        file_names = []
+        amazon_caller = boto3.resource("s3")
+        deliverable_bucket_name = self.generate_bucket_name(request.user)
+        amazon_caller.create_bucket(Bucket=deliverable_bucket_name)
+        deliverable_bucket = amazon_caller.Bucket(deliverable_bucket_name)
+        
+        # Fill in the bucket key names
+        for object in deliverable_bucket.objects.all():
+            file_names.append({"key":object.key,"qs":urllib.parse.quote_plus(object.key)})
+            object.Acl().put(ACL="public-read")
+        
+        context["file_names"] = file_names
+        context["deliverable_bucket"] = deliverable_bucket
+        
+        return context
+    
+    def generate_bucket_name(self,user):
+        return ("inflow-user-bucket-%s" % (user.id))
