4895fe89bf2a4cb8552502a94c83e9af03e792ae
Brian Katchmar <brian@workinflow.co>
2017-12-17 21:22:46 -0500

    Adding in new URLs for the Demo that will act as Email Tempaltes

--- inflowhq/inflowdemo/urls.py
+++ inflowhq/inflowdemo/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "htmldemos"
 urlpatterns = [
     url(r'^$', views.DemoLoginView.as_view(), name='demo_home'),
+    url(r'^myprojects/create-contract/freelancer-msg', views.DemoCreateContractFreelancer.as_view(), name='demo_create_contract_freelancer'),
+    url(r'^myprojects/create-contract/client-msg', views.DemoCreateContractClient.as_view(), name='demo_create_contract_freelancer'),
     url(r'^myprojects/create-contract/', views.DemoCreateNewContract.as_view(), name='demo_create_contract'),
     url(r'^myprojects/', views.DemoMyProjectsScreen.as_view(), name='demo_my_projects'),
 ]
--- inflowhq/inflowdemo/views.py
+++ inflowhq/inflowdemo/views.py
@@ -23,11 +23,25 @@ class DemoCreateNewContract(TemplateView):
     template_name = "createcontract.html"
     
     def get(self, request):
-        print(self.template_name)
-        print("I need to print this to see if this will even work here")
         return render(request, self.template_name)
     
     def post(self, request):
-        print(self.template_name)
-        print("I need to print this to see if this will even work here")
+        return render(request, self.template_name)
+    
+class DemoCreateContractClient(TemplateView):
+    template_name = "contract.creation.confirm.client.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        return render(request, self.template_name)
+    
+class DemoCreateContractFreelancer(TemplateView):
+    template_name = "contract.creation.confirm.freelancer.html"
+    
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request):
         return render(request, self.template_name)
