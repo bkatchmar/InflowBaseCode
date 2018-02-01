b14dd12c6d034628ba0293827b63c189135b6593
Brian Katchmar <brian@workinflow.co>
2018-02-01 01:51:38 -0500

    Changing up the S3 Upload based on all of the struggles I had to deal with tonight

--- inflowhq/inflowdemo/views.py
+++ inflowhq/inflowdemo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-import base64, urllib.request, mimetypes
+import base64, mimetypes
+import boto3
+import urllib.request
 
 class DemoLoginView(TemplateView):
     template_name = "demologin.html"
@@ -177,7 +179,52 @@ class DemoUploadMilestone(TemplateView):
 class DemoPreviewMilestone(TemplateView):
     template_name = "project.preview.milestone.html"
 
-    def get_context_data(self, **kwargs):
+    def get(self, request):
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        context = {}
+        
+        # Get data from the POST
+        uploaded_file = request.FILES.get("deliverable", False)
+        
+        # If we have an actual file, time to prepare it to be uploaded to AWS
+        if uploaded_file != False:
+            context = { "useDefault" : False }
+            deliverable_key = uploaded_file.__str__()
+            amazon_destination_bucket_name = "inflow-upload-demo"
+            amazon_caller_resource = boto3.resource("s3", region_name="us-east-1",aws_access_key_id="AKIAIQKGNH2YH2ZD2DOQ", aws_secret_access_key="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")
+            amazon_caller_client = boto3.client("s3", region_name="us-east-1",aws_access_key_id="AKIAIQKGNH2YH2ZD2DOQ", aws_secret_access_key="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")
+            
+            # Determine if we have to create a new bucket
+            bucket_list = amazon_caller_client.list_buckets()
+            need_to_create_bucket = True
+            for bucket in bucket_list["Buckets"]:
+                if bucket["Name"] == amazon_destination_bucket_name:
+                    need_to_create_bucket = False
+            
+            # If we need to create a new bucket, do so
+            # TO DO: Make this a more streamlined process
+            if need_to_create_bucket:
+                amazon_caller_resource.create_bucket(Bucket=amazon_destination_bucket_name)
+
+            # Place the uploaded file in Amazon
+            amazon_caller_resource.Bucket(amazon_destination_bucket_name).put_object(ACL="public-read", Key=deliverable_key, Body=uploaded_file)
+            
+            # Finally, get the newly created URL for the image and base64 encode it
+            path = ("https://s3.amazonaws.com/%s/%s" % (amazon_destination_bucket_name, deliverable_key))
+            print(path)
+            mime = mimetypes.guess_type(path)
+            image = urllib.request.urlopen(path)
+            image_64 = base64.encodestring(image.read())
+            context["imgData"] = u'data:%s;base64,%s' % (mime[0], str(image_64,"utf-8").replace("\n", ""))
+        else:
+            context = self.get_context_data(request)
+        
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, request, **kwargs):
         path = "https://www.fuzzyduk.com/wp-content/uploads/2017/04/MIN01WH.jpg"
         mime = mimetypes.guess_type(path)
         image = urllib.request.urlopen(path)
@@ -185,5 +232,6 @@ class DemoPreviewMilestone(TemplateView):
         
         # Call the base implementation first to get a context
         context = super(DemoPreviewMilestone, self).get_context_data(**kwargs)
+        context["useDefault"] = True
         context["imgData"] = u'data:%s;base64,%s' % (mime[0], str(image_64,"utf-8").replace("\n", ""))
         return context
