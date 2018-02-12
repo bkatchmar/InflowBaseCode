68cee78d67ed4897ed546a2f09be553a6c31ba59
Brian Katchmar <brian@workinflow.co>
2018-02-11 23:30:09 -0500

    Starting with High Fidelity Designs and point repository level static files to our AWS bucket

--- inflowhq/inflowco/urls.py
+++ inflowhq/inflowco/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.LoginView.as_view(), name='login'),
     url(r'^currencies/', views.CurrencyListView.as_view(), name='currencylistview'),
     url(r'^linkedinhandler/', views.LinkedInHandler.as_view(), name='linkedin'),
     url(r'^amazonboto/', views.AmazonBotoExamples.as_view(), name='amazonboto'),
--- inflowhq/inflowhq/urls.py
+++ inflowhq/inflowhq/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from inflowco.views import BaseSitemap, GoogleDomainVerificationFile
+from inflowco.views import BaseSitemap, LoginView, GoogleDomainVerificationFile
 
 sitemaps = {
     'static': BaseSitemap,
 }
 
 urlpatterns = [
+    url(r'^$', LoginView.as_view(), name='login'),
     url(r'^google255e09f84b6b193b\.html$', GoogleDomainVerificationFile.as_view(), name='google_domain_verification'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^inflow/', include('inflowco.urls')),
     url(r'^inflow/account/', include('accounts.urls')),
     url(r'^inflow/projects/', include('contractsandprojects.urls')),
     url(r'^inflow/stripe/', include('talktostripe.urls')),
     url(r'^inflow/demo/', include('inflowdemo.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    url(r'^admin/', admin.site.urls)
 ]
