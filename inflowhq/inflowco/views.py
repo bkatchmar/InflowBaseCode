e9363f6eed28b791293415f23793044da38ba345
Brian Katchmar <brian@workinflow.co>
2018-04-10 23:27:56 -0400

    Final Splash Page Updates from my talks with Kenny

--- inflowhq/inflowco/views.py
+++ inflowhq/inflowco/views.py
@@ -20,6 +20,7 @@ class IndexView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        context["active_link"] = "home"
         return context
     
     def get(self, request):
