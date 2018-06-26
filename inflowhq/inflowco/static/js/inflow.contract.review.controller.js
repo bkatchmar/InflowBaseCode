740e7bad5af34b18baaab5fee20bbdbbcfc621b9
Brian Katchmar <bkatchmar@gmail.com>
2018-06-25 22:11:44 -0400

    Starting up with basic interactivity with the page

--- inflowhq/inflowco/static/js/inflow.contract.review.controller.js
+++ inflowhq/inflowco/static/js/inflow.contract.review.controller.js
@@ -1,3 +1,20 @@
 reviewApp.controller("contractReviewCtrl", function($scope) {
-	
+	$scope.initArrayVals = function() {
+        $scope.editLabels = ["Edit"];
+    };
+    $scope.changeLabels = function(index) {
+        // Change the label
+        if ($scope.editLabels[index] === "Edit") {
+            $scope.editLabels[index] = "Finish Editing";
+        } else {
+            $scope.editLabels[index] = "Edit";
+        }
+    };
+    $scope.changeMode = function(mode) {
+        if ($scope.editMode === mode) {
+            $scope.editMode = "";
+        } else {
+            $scope.editMode = mode;
+        }
+    };
 });
