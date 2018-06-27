a7d7509afc1c343501ada479fc0f46b718ccf38a
Brian Katchmar <bkatchmar@gmail.com>
2018-06-26 22:32:53 -0400

    Some more AngularJS logic for the amendment screen

--- inflowhq/inflowco/static/js/inflow.contract.review.controller.js
+++ inflowhq/inflowco/static/js/inflow.contract.review.controller.js
@@ -1,6 +1,7 @@
 reviewApp.controller("contractReviewCtrl", function($scope) {
 	$scope.initArrayVals = function() {
         $scope.editLabels = ["Edit"];
+        $scope.editObjects = [];
     };
     $scope.changeLabels = function(index) {
         // Change the label
@@ -8,13 +9,29 @@ reviewApp.controller("contractReviewCtrl", function($scope) {
             $scope.editLabels[index] = "Finish Editing";
         } else {
             $scope.editLabels[index] = "Edit";
+            $scope.editMode = "";
+            $scope.whatWeAreEditing = "";
         }
     };
     $scope.changeMode = function(mode) {
         if ($scope.editMode === mode) {
             $scope.editMode = "";
+            $scope.whatWeAreEditing = "";
         } else {
             $scope.editMode = mode;
         }
+    };
+    $scope.changeWhatEditing = function(what) {
+        if ($scope.whatWeAreEditing === what) {
+            $scope.whatWeAreEditing = "";
+        } else {
+            $scope.whatWeAreEditing = what;
+        }
+    };
+    $scope.cancelEdit = function() {
+        $scope.whatWeAreEditing = "";
+    };
+    $scope.doesTheCurrentEditObjectsContainElement = function(field) {
+        return false;
     };
 });
