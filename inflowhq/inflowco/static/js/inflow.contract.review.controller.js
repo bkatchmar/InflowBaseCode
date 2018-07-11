ec52a06d850f78b6b6ba9f5538025d638066748c
Brian Katchmar <bkatchmar@gmail.com>
2018-07-10 22:14:34 -0400

    Starting with filling in the visual side of things, gotta go ahead and get to filling in the hidden forms

--- inflowhq/inflowco/static/js/inflow.contract.review.controller.js
+++ inflowhq/inflowco/static/js/inflow.contract.review.controller.js
@@ -34,21 +34,23 @@ reviewApp.controller("contractReviewCtrl", function($scope) {
         $scope.proposedChangeReason = "";
     };
     $scope.appendEdit = function() {
-        if ($scope.doesTheCurrentEditObjectsContainElement($scope.whatWeAreEditing)) {
-            var index = $scope.whatIndexDoesTheFieldHaveInTheCollection($scope.whatWeAreEditing);
-            $scope.editObjects[index]["newValue"] = $scope.proposedChangeValue;
-            $scope.editObjects[index]["newValueReason"] = $scope.proposedChangeReason;
-        } else {
-            $scope.editObjects.push(
-                {
-                    "fieldName" : $scope.whatWeAreEditing,
-                    "newValue" : $scope.proposedChangeValue,
-                    "newValueReason" : $scope.proposedChangeReason
-                }
-            );
+        // First check if we even need to run these checks, otherwise the user clearly didn't bother putting something in
+        if ($scope.entryIsValid()) {
+            if ($scope.doesTheCurrentEditObjectsContainElement($scope.whatWeAreEditing)) {
+                var index = $scope.whatIndexDoesTheFieldHaveInTheCollection($scope.whatWeAreEditing);
+                $scope.editObjects[index]["newValue"] = $scope.proposedChangeValue;
+                $scope.editObjects[index]["newValueReason"] = $scope.proposedChangeReason;
+            } else {
+                $scope.editObjects.push(
+                    {
+                        "fieldName" : $scope.whatWeAreEditing,
+                        "newValue" : $scope.proposedChangeValue,
+                        "newValueReason" : $scope.proposedChangeReason
+                    }
+                );
+            }
         }
         
-        console.log($scope.editObjects);
         $scope.cancelEdit();
     };
     $scope.doesTheCurrentEditObjectsContainElement = function(field) {
@@ -69,4 +71,25 @@ reviewApp.controller("contractReviewCtrl", function($scope) {
 
         return -1;
     };
+    $scope.entryIsValid = function() {
+        return $scope.proposedChangeValue != "" && $scope.proposedChangeReason != "";
+    };
+    $scope.getProposedNewValue = function(field){
+        for (var iterator = 0; iterator < $scope.editObjects.length; iterator++) {
+            if ($scope.editObjects[iterator]["fieldName"] === field) {
+                return $scope.editObjects[iterator]["newValue"];
+            }
+        }
+
+        return "";
+    }
+    $scope.getProposedDescription = function(field){
+        for (var iterator = 0; iterator < $scope.editObjects.length; iterator++) {
+            if ($scope.editObjects[iterator]["fieldName"] === field) {
+                return $scope.editObjects[iterator]["newValueReason"];
+            }
+        }
+
+        return "";
+    }
 });
